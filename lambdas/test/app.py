69b6ad40939337f29d7a943af0e7afbbc701d77f
Anastasiia Diudina <anastasiia@holvi.com>
2017-06-12 16:35:56 +0300

    Test modification for pull request builder checks

--- lambdas/test/app.py
+++ lambdas/test/app.py
@@ -5,7 +5,7 @@ app = Chalice(app_name='test')
 
 @app.route('/')
 def index():
-    return {'hello': 'world-test'}
+    return {'hello': 'world-test-agin'}
 
 
 # The view function above will return {"hello": "world"}

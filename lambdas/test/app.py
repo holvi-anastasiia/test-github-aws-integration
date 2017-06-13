acb2371919a9708c04f80daaae500faa0ec9ae6a
Anastasiia Diudina <anastasiia@holvi.com>
2017-06-13 14:06:53 +0300

    Test build again

--- lambdas/test/app.py
+++ lambdas/test/app.py
@@ -5,7 +5,7 @@ app = Chalice(app_name='test')
 
 @app.route('/')
 def index():
-    return {'hello': 'world-test-again'}
+    return {'hello-test': 'world-test-again'}
 
 
 # The view function above will return {"hello": "world"}
